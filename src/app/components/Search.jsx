2780ef13524d6663523a58747e5c619a412bb9d3
Lucas Chin <chinlucas4609@gmail.com>
2025-06-06 09:26:49 -0400

    okay

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -32,8 +32,6 @@ const Search = () => {
       setAccessToken(token);
     }
 
-    console.log(access_token);
-
     const tokenURL = `https://api.spotify.com/v1/search?q=${search}&type=track`;
 
     const response = await axios.get(tokenURL, {
@@ -41,7 +39,7 @@ const Search = () => {
         Authorization: `Bearer ${access_token}`,
       },
     });
-    console.log(response);
+
     trackList = response.data.tracks.items.map((track) => ({
       name: track.name,
       artist: track.artists[0].name,
